e91cd642dda8f9ee5558bf112a080d6f81bdf1e7
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2019-11-19 07:15:53 +0900

    new commit

--- date_the_image.py
+++ date_the_image.py
@@ -1,5 +1,21 @@
 from PIL import Image
+from PIL.ExifTags import TAGS
 
 def date_the_image(src: str, desc: str) -> None:
     im = Image.open(src)
+    
+    try:
+    	exif = im._getexif()
+    except AttributeError:
+    	return {}
+    	
+    exif_table = {}
+    for tag_id, value in exif.items():
+    	tag = TAGS.get(tag_id, tag_id)
+    	exif_table[tag] = value
+
+    return exif_table.get("DateTimeOriginal")
+    
     im.save(desc)
+    
+    print date_the_image(src)
