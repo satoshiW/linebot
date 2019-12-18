4a60efa019abbc4e0b0e7a7bd160e42831578dbd
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2019-12-19 07:19:23 +0900

    new commit

--- main.py
+++ main.py
@@ -70,30 +70,7 @@ def handle_image(event):
     # 画像をHerokuへ保存
     save_image(message_id, src_image_path)
 
-@handler.add(MessageEvent, message=TextMessage)    
-    def webhook(event):
-        date_picker = TemplateSendMessage(
-            alt_text='撮影日を選択',
-            template=ButtonsTemplate(
-                text='撮影日を選択',
-                title='YYYY-MM-dd',
-                actions=[
-                    DatetimePickerTemplateAction(
-                        label='選択',
-                        data='action=buy&itemid=1',
-                        mode='date',
-                        initial='2017-04-01',
-                        min='2017-04-01',
-                        max='2099-12-31'
-                    )
-                ]
-            )
-        )
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            date_picker
-        )
+    webhook(event)
     
     date_the_image(src_image_path, Path(main_image_path).absolute())
     date_the_image(src_image_path, Path(preview_image_path).absolute())
@@ -140,7 +117,7 @@ def save_image(message_id: str, save_path: str) -> None:
            HttpMethod = "GET"
     )"""
 
-def webhook():
+def webhook(event):
     date_picker = TemplateSendMessage(
         alt_text='撮影日を選択',
         template=ButtonsTemplate(
