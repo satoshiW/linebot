5dba697f7ae520127ec312006b4bdf0ba3eb1678
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2019-12-25 07:31:38 +0900

    new commit

--- main.py
+++ main.py
@@ -93,10 +93,10 @@ def handle_image(event):
         date_picker
     )
 
-
+ @handler.add(PostbackEvent)
+ def handle_postback(event):
     date_the_image(src_image_path, Path(main_image_path).absolute())
     date_the_image(src_image_path, Path(preview_image_path).absolute())
-    
     
     """
     image_message = ImageSendMessage(
