deb047adafe3d1e5f71fbfd81629ec9e0c1eaa13
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2019-12-25 17:53:12 +0900

    new commit

--- main.py
+++ main.py
@@ -95,6 +95,11 @@ def handle_image(event):
 
     @handler.add(PostbackEvent)
     def handle_postback(event):
+        line_bot_api.reply_message(
+            event.reply_token,
+            text=event.postback.params['date']
+        )
+
         date_the_image(src_image_path, Path(main_image_path).absolute())
         date_the_image(src_image_path, Path(preview_image_path).absolute())
     
@@ -145,11 +150,7 @@ def date_the_image(src: str, desc: str) -> None:
     im = Image.open(src)
     draw = ImageDraw.Draw(im)
     font = ImageFont.truetype("./fonts/Helvetica.ttc", 60)
-    text = line_bot_api.reply_message(
-                event.reply_token,
-                event.postback.params['date']
-                )
-    
+
     x = 10
     y = 10
     margin = 5
