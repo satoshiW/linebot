cf2f9a08e0a66aa6e3e535758e67ffcea76c7147
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2020-01-23 22:21:08 +0900

    new commit

--- main.py
+++ main.py
@@ -162,8 +162,8 @@ def date_the_image(src: str, desc: str, event) -> None:
     x = im.width - text_width
     y = im.height - text_height
     draw.rectangle(
-            (x - margin * 6, y - margin * 3, im.width, im.height - margin), fill=(0, 0, 0, 127)
-        ).convert("RGBA")
+            (x - margin * 6, y - margin * 3, im.width, im.height - margin), fill=(0, 0, 0)
+        ).putalpha(128)
     draw.text((x - margin * 3, y - margin * 2), text, fill=(255, 255, 255), font=font)
         
     im.save(desc)
