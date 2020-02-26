b4626b74785af0ccec19ac1fed7546576c315a84
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2020-02-27 07:21:46 +0900

    new commit

--- database.py
+++ database.py
@@ -23,78 +23,78 @@ def get_data(event, user_id):
 		name_1 = cursor.fetchone(cursor["name"])
 		buttons_template = ButtonTemplate(
         text="誰が写ってる？", actions=[
-            MessageAction(label=name_1, text=name_1),
-            MessageAction(label="その他", text="その他")
-        ])
-        get_day()
-    #2人登録の場合
-    elif cursor == 2:
-    	cursor.execute("SELECT * FROM user WHERE user_id=user_id")
-        name_1 = cursor.fetchone(cursor["name"])
-        name_2 = cursor.fetchone(cursor["name"])
-        buttons_template = ButtonTemplate(
-        text="誰が写ってる？", actions=[
-            MessageAction(label=name_1, text=name_1),
-            MessageAction(label=name_2, text=name_2),
-            MessageAction(label="その他", text="その他")
-        ])
-        get_day()
-    #３人登録の場合
-    else:
-    	cursor.execute("SELECT * FROM user WHERE user_id=user_id")
-        name_1 = cursor.fetchone(cursor["name"])
-        name_2 = cursor.fetchone(cursor["name"])
-        name_3 = cursor.fetchone(cursor["name"])
-        buttons_template = ButtonTemplate(
-        text="誰が写ってる？", actions=[
-            MessageAction(label=name_1, text=name_1),
-            MessageAction(label=name_2, text=name_2),
-            MessageAction(label=name_3, text=name_3),
-            MessageAction(label="その他", text="その他")
-        ])
-        if event.message.text == "その他":
-	        #名前の確認
+			MessageAction(label=name_1, text=name_1),
+			MessageAction(label="その他", text="その他")
+		])
+		get_day()
+	#2人登録の場合
+	elif cursor == 2:
+		cursor.execute("SELECT * FROM user WHERE user_id=user_id")
+		name_1 = cursor.fetchone(cursor["name"])
+		name_2 = cursor.fetchone(cursor["name"])
+		buttons_template = ButtonTemplate(
+		text="誰が写ってる？", actions=[
+			MessageAction(label=name_1, text=name_1),
+			MessageAction(label=name_2, text=name_2),
+			MessageAction(label="その他", text="その他")
+		])
+		get_day()
+	#３人登録の場合
+	else:
+		cursor.execute("SELECT * FROM user WHERE user_id=user_id")
+		name_1 = cursor.fetchone(cursor["name"])
+		name_2 = cursor.fetchone(cursor["name"])
+		name_3 = cursor.fetchone(cursor["name"])
+		buttons_template = ButtonTemplate(
+		text="誰が写ってる？", actions=[
+			MessageAction(label=name_1, text=name_1),
+			MessageAction(label=name_2, text=name_2),
+			MessageAction(label=name_3, text=name_3),
+			MessageAction(label="その他", text="その他")
+		])
+		if event.message.text == "その他":
+			#名前の確認
 			line_bot_api.reply_message(
-        		event.reply_token,
-        		TextSendMessage(text="写真に写っている人の名前は？"))
-    		#名前をnameに代入
+				event.reply_token,
+				TextSendMessage(text="写真に写っている人の名前は？"))
+			#名前をnameに代入
 			text_name = event.message.text
 			#生年月日の確認
 			line_bot_api.reply_message(
-        		event.reply_token,
-        		TextSendMessage(text=text_name+"さんの生年月日を◯◯◯◯-◯◯-◯◯の形式で入力してね"))
-    		#生年月日をdayに代入
-    		birthday = event.message.text
-    	else:
-    		text_name = event.message.text
-        	corsor.execute("SELECT day FROM user WHERE user_id=user_id and name=text_name")
-        	birthday = cursor["day"]
+				event.reply_token,
+				TextSendMessage(text=text_name+"さんの生年月日を◯◯◯◯-◯◯-◯◯の形式で入力してね"))
+			#生年月日をdayに代入
+			birthday = event.message.text
+		else:
+			text_name = event.message.text
+			corsor.execute("SELECT day FROM user WHERE user_id=user_id and name=text_name")
+			birthday = cursor["day"]
 
 def update_data():
 	#名前の確認
 	line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="写真に写っている人の名前は？"))
-    #名前をnameに代入
+		event.reply_token,
+		TextSendMessage(text="写真に写っている人の名前は？"))
+	#名前をnameに代入
 	text_name = event.message.text
 	#nameを更新
 	cursor.execute("UPDATE user SET name=text_name WHERE user_id=user_id")
 	#生年月日の確認
 	line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=text_name+"さんの生年月日を◯◯◯◯-◯◯-◯◯の形式で入力してね"))
-    #生年月日をdayに代入
-    birthday = event.message.text
-    #dayを更新
+		event.reply_token,
+		TextSendMessage(text=text_name+"さんの生年月日を◯◯◯◯-◯◯-◯◯の形式で入力してね"))
+	#生年月日をdayに代入
+	birthday = event.message.text
+	#dayを更新
 	cursor.execute("UPDATE user SET day=birthday WHERE user_id=user_id")
 
 def get_day():
 	if event.message.text == "その他":
-        update_data()
-    else:
-    	text_name = event.message.text
-        corsor.execute("SELECT day FROM user WHERE user_id=user_id and name=text_name")
-        birthday = corsor["day"]
+		update_data()
+	else:
+		text_name = event.message.text
+		corsor.execute("SELECT day FROM user WHERE user_id=user_id and name=text_name")
+		birthday = corsor["day"]
 
 connection.commit()
 connection.close()
