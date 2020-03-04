2bb4633e805274242b1a94c6da454526f7e3aca4
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2020-03-04 22:23:57 +0900

    new commit

--- database.py
+++ database.py
@@ -11,15 +11,15 @@ cursor = connection.cursor(MySQLdb.cursors.DictCursor)
 
 def get_data(event, user_id):
 	#user_idの参照
-	cursor.execute("SELECT COUNT(user_id=user_id) FROM user")
+	cursor.execute("""SELECT COUNT(user_id=user_id) FROM user""")
 	#user_idが無かった場合
 	if cursor == 0:
 		#user_idを追加
-		cursor.execute("INSERT INTO user VALUES user_id")
+		cursor.execute("""INSERT INTO user VALUES user_id""")
 		update_data()
 	#1人登録の場合
 	elif cursor == 1:
-		cursor.execute("SELECT * FROM user WHERE user_id=user_id")
+		cursor.execute("""SELECT * FROM user WHERE user_id=user_id""")
 		name_1 = cursor.fetchone(cursor["name"])
 		buttons_template = ButtonTemplate(
         text="誰が写ってる？", actions=[
@@ -29,7 +29,7 @@ def get_data(event, user_id):
 		get_day()
 	#2人登録の場合
 	elif cursor == 2:
-		cursor.execute("SELECT * FROM user WHERE user_id=user_id")
+		cursor.execute("""SELECT * FROM user WHERE user_id=user_id""")
 		name_1 = cursor.fetchone(cursor["name"])
 		name_2 = cursor.fetchone(cursor["name"])
 		buttons_template = ButtonTemplate(
@@ -41,7 +41,7 @@ def get_data(event, user_id):
 		get_day()
 	#３人登録の場合
 	else:
-		cursor.execute("SELECT * FROM user WHERE user_id=user_id")
+		cursor.execute("""SELECT * FROM user WHERE user_id=user_id""")
 		name_1 = cursor.fetchone(cursor["name"])
 		name_2 = cursor.fetchone(cursor["name"])
 		name_3 = cursor.fetchone(cursor["name"])
@@ -67,7 +67,7 @@ def get_data(event, user_id):
 			birthday = event.message.text
 		else:
 			text_name = event.message.text
-			corsor.execute("SELECT day FROM user WHERE user_id=user_id and name=text_name")
+			corsor.execute("""SELECT day FROM user WHERE user_id=user_id and name=text_name""")
 			birthday = cursor["day"]
 
 def update_data():
@@ -78,7 +78,7 @@ def update_data():
 	#名前をnameに代入
 	text_name = event.message.text
 	#nameを更新
-	cursor.execute("UPDATE user SET name=text_name WHERE user_id=user_id")
+	cursor.execute("""UPDATE user SET name=text_name WHERE user_id=user_id""")
 	#生年月日の確認
 	line_bot_api.reply_message(
 		event.reply_token,
@@ -86,14 +86,14 @@ def update_data():
 	#生年月日をdayに代入
 	birthday = event.message.text
 	#dayを更新
-	cursor.execute("UPDATE user SET day=birthday WHERE user_id=user_id")
+	cursor.execute("""UPDATE user SET day=birthday WHERE user_id=user_id""")
 
 def get_day():
 	if event.message.text == "その他":
 		update_data()
 	else:
 		text_name = event.message.text
-		corsor.execute("SELECT day FROM user WHERE user_id=user_id and name=text_name")
+		corsor.execute("""SELECT day FROM user WHERE user_id=user_id and name=text_name""")
 		birthday = corsor["day"]
 
 connection.commit()
