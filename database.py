32bfeb906827853679cbd3ef60bb76b9ed42edfa
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2020-04-06 22:47:19 +0900

    new commit

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy, create_engine
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 engine = create_engine("postgres://vmmfszxyquhraz:dde9deee8b575db7a8f4214d70e99429a5bb1a73d018ce8665642754005ed4ed@ec2-52-86-73-86.compute-1.amazonaws.com:5432/d1l9tnctjr6utu")
 #app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
