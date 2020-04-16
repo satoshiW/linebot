46b7ceee12566cf5fdf406ee4d1cd37150cbcc0f
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2020-04-16 23:50:03 +0900

    new commit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from flask import Flask, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, Column, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (FollowEvent, PostbackEvent, TemplateSendMessage, ButtonsTemplate, DatetimePickerTemplateAction, ImageMessage, ImageSendMessage, MessageEvent, TextMessage, TextSendMessage)
@@ -8,7 +13,7 @@ import datetime
 import os
 import re
 
-import database
+#import database
 
 app = Flask(__name__)
 app.debug = False
@@ -20,6 +25,18 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+engine = create_engine("postgresql://vmmfszxyquhraz:dde9deee8b575db7a8f4214d70e99429a5bb1a73d018ce8665642754005ed4ed@ec2-52-86-73-86.compute-1.amazonaws.com:5432/d1l9tnctjr6utu")
+Base = declarative_base()
+
+class User(Base):
+	__tablename__ = "user_list"
+	user_id = Column("user_id", String(50), primary_key=True)
+	name = Column("name", String(10))
+	day = Column("day", Date)
+
+Base.metadata.create_all(engine)
+session = Session(bind=engine)
+
 #画像の参照元パス
 SRC_IMAGE_PATH = "static/images/{}.jpg"
 MAIN_IMAGE_PATH = "static/images/{}_main.jpg"
@@ -27,6 +44,7 @@ PREVIEW_IMAGE_PATH = "static/images/{}_preview.jpg"
 
 #message_idを格納する為のリスト
 message_list = []
+name_list = []
 user_dict = {}
 
 @app.route("/callback", methods=['POST'])
@@ -71,7 +89,14 @@ def handle_image(event):
     im = Image.open(src_image_path)
     im.save(src_image_path)
     
-    database.get_data(event, user_id, line_bot_api)
+    users = session.query(User).filter(User.user_id==f'{user_id}').all()
+	for row in users:
+		name_list.append(row.name)
+	#user_idの参照
+	#res = session.query(User).filter(User.user_id==f'{user_id}').count()
+	num = len(name_list)
+    
+    #database.get_data(event, user_id, line_bot_api)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text(event):
@@ -81,6 +106,80 @@ def handle_text(event):
     user_id = event.source.user_id
     #ファイル名をmessage_idに変換したパス
     src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
+    
+    #user_idが無かった場合
+	if num == 0:
+		#user_idを追加
+		user1 = User(user_id=f"{user_id}")
+		session.add(user1)
+		#名前の確認
+	line_bot_api.reply_message(
+		event.reply_token,
+		TextSendMessage(text="写真に写っている人の名前は？"))
+	#名前をnameに代入
+	text_name = event.message.text
+	#nameを更新
+	user_name = session.query(User).filter(User.user_id==f"user_id").first()
+	user_name.name = text_name
+	#生年月日の確認
+	line_bot_api.reply_message(
+		event.reply_token,
+		TextSendMessage(text=text_name+"さんの生年月日を◯◯◯◯-◯◯-◯◯の形式で入力してね"))
+	#生年月日をdayに代入
+	birthday = event.message.text
+	#dayを更新
+	user_day = session.query(User).filter(User.user_id==f"user_id").first()
+	user_day.day = birthday
+	#1人登録の場合
+	elif num == 1:
+		#users = session.query(User).filter(User.user_id==f'{user_id}').first()
+		name_1 = name_list[0]
+		buttons_template = ButtonTemplate(
+        text="誰が写ってる？", actions=[
+			MessageAction(label=name_1, text=name_1),
+			MessageAction(label="その他", text="その他")
+		])
+		get_day()
+	#2人登録の場合
+	elif num == 2:
+		name_1 = name_list[0]
+		name_2 = name_list[1]
+		buttons_template = ButtonTemplate(
+		text="誰が写ってる？", actions=[
+			MessageAction(label=name_1, text=name_1),
+			MessageAction(label=name_2, text=name_2),
+			MessageAction(label="その他", text="その他")
+		])
+		get_day()
+	#３人登録の場合
+	elif num == 3:
+		name_1 = name_list[0]
+		name_2 = name_list[1]
+		name_3 = name_list[2]
+		buttons_template = ButtonTemplate(
+		text="誰が写ってる？", actions=[
+			MessageAction(label=name_1, text=name_1),
+			MessageAction(label=name_2, text=name_2),
+			MessageAction(label=name_3, text=name_3),
+			MessageAction(label="その他", text="その他")
+		])
+		if event.message.text == "その他":
+			#名前の確認
+			line_bot_api.reply_message(
+				event.reply_token,
+				TextSendMessage(text="写真に写っている人の名前は？"))
+			#名前をnameに代入
+			text_name = event.message.text
+			#生年月日の確認
+			line_bot_api.reply_message(
+				event.reply_token,
+				TextSendMessage(text=text_name+"さんの生年月日を◯◯◯◯-◯◯-◯◯の形式で入力してね"))
+			#生年月日をdayに代入
+			birthday = event.message.text
+		else:
+			text_name = event.message.text
+			res = session.query(User).filter(User.user_id==f'{user_id}', User.name==text_name).first()
+			birthday = res
     
     #user_dict[text_name] = birthday
     
@@ -141,6 +240,14 @@ def save_image(message_id: str, save_path: str) -> None:
         # 取得したバイナリデータを書き込む
         for chunk in message_content.iter_content():
             f.write(chunk)
+
+def get_day():
+	if event.message.text == "その他":
+		update_data(event, user_id, line_bot_api)
+	else:
+		text_name = event.message.text
+		res = session.query(User).filter(User.user_id==f'{user_id}', User.name==text_name).first()
+		birthday = res
 
 #画像処理関数
 def date_the_image(src: str, desc: str, event) -> None:
