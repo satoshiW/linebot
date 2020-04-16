4ef6191d75fc3249a30262b01ad247af77f03a97
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2020-04-17 00:00:37 +0900

    new commit

--- main.py
+++ main.py
@@ -92,9 +92,9 @@ def handle_image(event):
     users = session.query(User).filter(User.user_id==f'{user_id}').all()
     for row in users:
         name_list.append(row.name)
-	#user_idの参照
-	#res = session.query(User).filter(User.user_id==f'{user_id}').count()
-	num = len(name_list)
+    #user_idの参照
+    #res = session.query(User).filter(User.user_id==f'{user_id}').count()
+    num = len(name_list)
     
     #database.get_data(event, user_id, line_bot_api)
 
@@ -108,78 +108,78 @@ def handle_text(event):
     src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
     
     #user_idが無かった場合
-	if num == 0:
-		#user_idを追加
-		user1 = User(user_id=f"{user_id}")
-		session.add(user1)
-		#名前の確認
-	line_bot_api.reply_message(
-		event.reply_token,
-		TextSendMessage(text="写真に写っている人の名前は？"))
-	#名前をnameに代入
-	text_name = event.message.text
-	#nameを更新
-	user_name = session.query(User).filter(User.user_id==f"user_id").first()
-	user_name.name = text_name
-	#生年月日の確認
-	line_bot_api.reply_message(
-		event.reply_token,
-		TextSendMessage(text=text_name+"さんの生年月日を◯◯◯◯-◯◯-◯◯の形式で入力してね"))
-	#生年月日をdayに代入
-	birthday = event.message.text
-	#dayを更新
-	user_day = session.query(User).filter(User.user_id==f"user_id").first()
-	user_day.day = birthday
-	#1人登録の場合
-	elif num == 1:
-		#users = session.query(User).filter(User.user_id==f'{user_id}').first()
-		name_1 = name_list[0]
-		buttons_template = ButtonTemplate(
+    if num == 0:
+        #user_idを追加
+        user1 = User(user_id=f"{user_id}")
+        session.add(user1)
+    #名前の確認
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="写真に写っている人の名前は？"))
+    #名前をnameに代入
+    text_name = event.message.text
+    #nameを更新
+    user_name = session.query(User).filter(User.user_id==f"user_id").first()
+    user_name.name = text_name
+    #生年月日の確認
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=text_name+"さんの生年月日を◯◯◯◯-◯◯-◯◯の形式で入力してね"))
+    #生年月日をdayに代入
+    birthday = event.message.text
+    #dayを更新
+    user_day = session.query(User).filter(User.user_id==f"user_id").first()
+    user_day.day = birthday
+    #1人登録の場合
+    elif num == 1:
+        #users = session.query(User).filter(User.user_id==f'{user_id}').first()
+        name_1 = name_list[0]
+        buttons_template = ButtonTemplate(
         text="誰が写ってる？", actions=[
-			MessageAction(label=name_1, text=name_1),
-			MessageAction(label="その他", text="その他")
-		])
-		get_day()
-	#2人登録の場合
-	elif num == 2:
-		name_1 = name_list[0]
-		name_2 = name_list[1]
-		buttons_template = ButtonTemplate(
-		text="誰が写ってる？", actions=[
-			MessageAction(label=name_1, text=name_1),
-			MessageAction(label=name_2, text=name_2),
-			MessageAction(label="その他", text="その他")
-		])
-		get_day()
-	#３人登録の場合
-	elif num == 3:
-		name_1 = name_list[0]
-		name_2 = name_list[1]
-		name_3 = name_list[2]
-		buttons_template = ButtonTemplate(
-		text="誰が写ってる？", actions=[
-			MessageAction(label=name_1, text=name_1),
-			MessageAction(label=name_2, text=name_2),
-			MessageAction(label=name_3, text=name_3),
-			MessageAction(label="その他", text="その他")
-		])
-		if event.message.text == "その他":
-			#名前の確認
-			line_bot_api.reply_message(
-				event.reply_token,
-				TextSendMessage(text="写真に写っている人の名前は？"))
-			#名前をnameに代入
-			text_name = event.message.text
-			#生年月日の確認
-			line_bot_api.reply_message(
-				event.reply_token,
-				TextSendMessage(text=text_name+"さんの生年月日を◯◯◯◯-◯◯-◯◯の形式で入力してね"))
-			#生年月日をdayに代入
-			birthday = event.message.text
-		else:
-			text_name = event.message.text
-			res = session.query(User).filter(User.user_id==f'{user_id}', User.name==text_name).first()
-			birthday = res
+            MessageAction(label=name_1, text=name_1),
+            MessageAction(label="その他", text="その他")
+        ])
+        get_day()
+    #2人登録の場合
+    elif num == 2:
+        name_1 = name_list[0]
+        name_2 = name_list[1]
+        buttons_template = ButtonTemplate(
+        text="誰が写ってる？", actions=[
+            MessageAction(label=name_1, text=name_1),
+            MessageAction(label=name_2, text=name_2),
+            MessageAction(label="その他", text="その他")
+        ])
+        get_day()
+    #３人登録の場合
+    elif num == 3:
+        name_1 = name_list[0]
+        name_2 = name_list[1]
+        name_3 = name_list[2]
+        buttons_template = ButtonTemplate(
+        text="誰が写ってる？", actions=[
+            MessageAction(label=name_1, text=name_1),
+            MessageAction(label=name_2, text=name_2),
+            MessageAction(label=name_3, text=name_3),
+            MessageAction(label="その他", text="その他")
+        ])
+        if event.message.text == "その他":
+            #名前の確認
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="写真に写っている人の名前は？"))
+            #名前をnameに代入
+            text_name = event.message.text
+            #生年月日の確認
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=text_name+"さんの生年月日を◯◯◯◯-◯◯-◯◯の形式で入力してね"))
+            #生年月日をdayに代入
+            birthday = event.message.text
+        else:
+            text_name = event.message.text
+            res = session.query(User).filter(User.user_id==f'{user_id}', User.name==text_name).first()
+            birthday = res
     
     #user_dict[text_name] = birthday
     
@@ -242,12 +242,12 @@ def save_image(message_id: str, save_path: str) -> None:
             f.write(chunk)
 
 def get_day():
-	if event.message.text == "その他":
-		update_data(event, user_id, line_bot_api)
-	else:
-		text_name = event.message.text
-		res = session.query(User).filter(User.user_id==f'{user_id}', User.name==text_name).first()
-		birthday = res
+    if event.message.text == "その他":
+        update_data(event, user_id, line_bot_api)
+    else:
+        text_name = event.message.text
+        res = session.query(User).filter(User.user_id==f'{user_id}', User.name==text_name).first()
+        birthday = res
 
 #画像処理関数
 def date_the_image(src: str, desc: str, event) -> None:
