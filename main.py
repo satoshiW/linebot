37b28134ec11a52a08ea1b59656a787b15b9718b
Satoshi Wakamatsu <sawakamatsu3@gmail.com>
2020-05-03 15:05:46 +0900

    new commit

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ def select_day(event):
         alt_text=message,
         template=ButtonsTemplate(
             text=message,
-            thumbnail_image_url="https://drive.google.com/drive/my-drive/chica_l.jpg",
+            #thumbnail_image_url=f"https://hidden-anchorage-52228.herokuapp.com/{src_image_path}",
             actions=[
                 DatetimePickerTemplateAction(
                     label="選択",
